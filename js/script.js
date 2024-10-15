750dff64095783547a22eebc3f7e3d1de4dad8f4
Johnny202406 <funnyvalentineofi@gmail.com>
2024-10-15 12:30:50 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -31,3 +31,39 @@ for(let index of descripcion) {
     index.innerHTML = index.innerHTML.toLowerCase()
     
 }
+
+
+// CARRUSEL
+const carrusel = document.querySelector('.carrusel');
+const images = document.querySelectorAll('.carrusel img');
+let currentIndex = 0;
+
+function moveTo(index) {
+    // Asegúrate de que el índice esté dentro de los límites
+    if (index < 0) {
+        index = images.length - 1; // Volver al último
+
+    } else if (index >= images.length) {
+        index = 0; // Volver al primero
+    }
+
+    // Calcula el desplazamiento y aplica la transformación
+    const scrollTo = index * carrusel.clientWidth;
+
+    carrusel.scrollTo({
+        left: scrollTo,
+        behavior: 'smooth'
+    });
+
+    // Actualiza el índice actual
+    currentIndex = index;
+}
+
+// Manejo de clics en las flechas
+document.querySelector('.izquierda').addEventListener('click', () => {
+    moveTo(currentIndex - 1);
+});
+
+document.querySelector('.derecha').addEventListener('click', () => {
+    moveTo(currentIndex + 1);
+});
